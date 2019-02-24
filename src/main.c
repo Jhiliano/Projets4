f886cf60ee6eaa86ef537e2e44ce745178681d66
Jhiliano <ggfourcroy@gmail.com>
2019-02-24 22:02:03 +0100

    -

--- src/main.c
+++ src/main.c
@@ -5,18 +5,16 @@
 
 /* dépendances */
 
-#include "../../headers/Raid5/couche1.h"
-#include "../../headers/Raid5/couche2.h"
-#include "../../headers/Raid5/couche3.h"
-#include "../../headers/Raid5/couche4.h"
-#include "../../headers/Raid5/couche5.h"
+#include "../headers/Raid5/couche1.h"
+#include "../headers/Raid5/couche2.h"
+#include "../headers/Raid5/couche3.h"
+#include "../headers/Raid5/couche4.h"
+#include "../headers/Raid5/couche5.h"
 
 /* fonctions */
 
-int main(int argc, char* argv[])
+int main(void)
 /// \brief Main
-/// \param[in] argc le nombre d'arguments
-/// \param[in] argv les arguments
 /// \return EXIT_SUCCESS le programme n'a pas eut d'encombre sinon il aurrai rencontré un exit(EXIT_FAILURE)
 {
   return EXIT_SUCCESS;
