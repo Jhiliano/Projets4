5b6a88feef2b54398193e59cee9d05846a8699b0
Pierre <pierre.rigal17@gmail.com>
2019-03-10 21:00:15 +0100

    Avancement c2
    couche 2, 3 premières fonctions
    
    Co-Authored-By: vibujor <vibujor@users.noreply.github.com>

--- src/Raid5/couche2.c
+++ src/Raid5/couche2.c
@@ -11,3 +11,24 @@
 
 
 /* fonctions */
+int compute_nstripe(int nb_blocks){
+  if (nb_blocks%r5Disk.ndisk == 0) return nb_blocks/r5Disk.ndisk;
+  return nb_blocks/r5Disk.ndisk+1;
+}
+
+block_t compute_parity(stripe_t bande, int posP){
+  int i;
+  block_t parite = bande.stripe[0];
+  for(i=1; i<bande.nblocks; i++){
+    if(i!=posP){
+      for(int e=0; e<BLOCK_SIZE; e++){
+        parite.data[e]=parite.data[e]^bande.stripe[i].data[e];
+      }
+    }
+  }
+  return parite;
+}
+
+int compute_parity_index(int numBande){
+  return (r5Disk.ndisk-1)-(numBande%r5Disk.ndisk);
+}
