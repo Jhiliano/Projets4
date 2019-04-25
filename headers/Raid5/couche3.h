68add18fe90e902227f7a4a939e677debfe98bda
Nibellung <pierre.rigal17@gmail.com>
2019-04-25 16:58:51 +0200

    Il manque cmd_dump_inode dans la couche 3 sinon c'est bon. Il faudra vérifié que ça marche.

--- headers/Raid5/couche3.h
+++ headers/Raid5/couche3.h
@@ -16,14 +16,14 @@
 
 /* Fonctions */
 
-void read_inodes_table();
+int read_inodes_table();
 void write_inodes_table();
 void delete_inode(int pos);
 int get_unused_inode();
 int init_inode(char *nomF, uint taille, uint pos);
 void cmd_dump_inode(char *nomR);
-void write_super_block();
-void read_super_block();
+int write_super_block();
+int read_super_block();
 void first_free_byte();
 
 #endif
--- src/Raid5/couche3.c
+++ src/Raid5/couche3.c
@@ -12,13 +12,46 @@
 
 /* fonctions */
 
-void read_inodes_table() {
+int read_inodes_table() {
+  /**
+  * \brief Lit la table d'inode dans le raid.
+  * \return 0 si tout c'est bien passé, ERR_READ si il a eu un problème de lecture.
+  */
 
+  int pos = INODES_START; // Position de départ du tableau d'inodes dans le raid.
+  int fichier; // fichier sur lequel lire un inode.
+
+  // Parcours du tableau d'inodes.
+  for (int i_inode = 0; i_inode < INODE_TABLE_SIZE; i_inode++) {
+    fichier = i_inode % r5Disk.ndisk; // On se place dans le bon fichier.
+    if (i_inode > 0 && fichier == 0) pos++; // Si on a lu dans tous les fichiers on incrémente pos.
+    fseek(r5Disk.storage[fichier], pos, SEEK_SET); // Placer le curseur sur pos dans le bon fichier.
+    if (fread(&r5Disk.inodes[i_inode], sizeof(struct inode_s), 1, r5Disk.storage[fichier]) != 1) return ERR_READ; // Lire l'inode.
+  }
+
+  return 0;
 }
+
+/*****************************************************************************************************************/
 
 void write_inodes_table() {
+  /**
+  * \brief Ecrit la table d'inode dans le raid.
+  */
 
+  int pos = INODES_START; // Position de départ du tableau d'inodes dans le raid.
+  int fichier; // fichier sur lequel écrire un inode.
+
+  // Parcours du tableau d'inodes.
+  for (int i_inode = 0; i_inode < INODE_TABLE_SIZE; i_inode++) {
+    fichier = i_inode % r5Disk.ndisk; // On se place dans le bon fichier.
+    if (i_inode > 0 && fichier == 0) pos++; // Si on a écrit dans tous les fichiers on incrémente pos.
+    fseek(r5Disk.storage[fichier], pos, SEEK_SET); // Placer le curseur sur pos dans le bon fichier.
+    fwrite(&r5Disk.inodes[i_inode], sizeof(struct inode_s), 1, r5Disk.storage[fichier]); // Ecrire l'inode.
+  }
 }
+
+/*****************************************************************************************************************/
 
 void delete_inode(int pos) {
   /**
@@ -33,6 +66,9 @@ void delete_inode(int pos) {
   if ((i_inode = get_unused_inode()) == -1)
     i_inode = INODE_TABLE_SIZE - 1;
 
+  // Mise à jour le super block.
+  r5Disk.super_block.nb_blocks_used -= r5Disk.inodes[pos].nblock;
+
   // On décale les inodes en arrière à partir de l'inode à supprimer.
   for (int i = pos; i < i_inode - 1; i++)
     r5Disk.inodes[i] = r5Disk.inodes[i + 1];
@@ -40,6 +76,8 @@ void delete_inode(int pos) {
   // On supprime le dernier inode pour ne pas l'avoir en double
   r5Disk.inodes[i_inode].first_byte = 0;
 }
+
+/*****************************************************************************************************************/
 
 int get_unused_inode() {
   /**
@@ -54,6 +92,8 @@ int get_unused_inode() {
   return -1;
 }
 
+/*****************************************************************************************************************/
+
 int init_inode(char *nomF, uint taille, uint pos) {
   /**
   * \brief Initialise un inode à partir de son nom, sa taille et sa position.
@@ -64,8 +104,8 @@ int init_inode(char *nomF, uint taille, uint pos) {
   * \return 0 si l'inode a été initialisé, 1 sinon.
   */
 
-  inode_t i;
-  int i_inode;
+  inode_t i; // Nouveau inode.
+  int i_inode; // Position à laquelle insérer cet inode.
 
   // On vérifie qu'il y a de la place pour un inode.
   if ((i_inode = get_unused_inode()) != -1) {
@@ -74,26 +114,62 @@ int init_inode(char *nomF, uint taille, uint pos) {
     i.size = taille;
     i.nblock = compute_nblock(taille);
     i.first_byte = pos;
+
     // On place l'inode dans la table
     r5Disk.inodes[i_inode] = i;
+
+    // Mise à jour le super block.
+    r5Disk.super_block.nb_blocks_used += i.nblock;
+
     return 0;
   }
 
   return 1;
 }
 
+/*****************************************************************************************************************/
+
 void cmd_dump_inode(char *nomR) {
 
 }
 
-void write_super_block() {
+/*****************************************************************************************************************/
 
+int write_super_block() {
+  /**
+  * \brief Ecrit le super block au début du raid.
+  * \return Le nombre d'élément écrit.
+  */
+
+  return (fwrite(&r5Disk.super_block, sizeof(struct super_block_s), 1, r5Disk.storage[1]));
 }
 
-void read_super_block() {
+/*****************************************************************************************************************/
 
+int read_super_block() {
+  /**
+  * \brief Lecture du super block.
+  * \details fread stocke lit le super block au début du raid et le stocke dans r5Disk.super_block.
+  * \return 0 si la lecture c'est bien passé, ERR_READ sinon.
+  */
+
+  if (fread(&r5Disk.super_block, sizeof(struct super_block_s), 1, r5Disk.storage[1]) != 1) return ERR_READ;
+	return 0;
 }
+
+/*****************************************************************************************************************/
 
 void first_free_byte() {
+  /**
+  * \brief Mise à jour du premier byte libre.
+  */
 
+  int i_inode; // Position du premier inode disponible dans le tableau.
+
+  // Si il n'y a pas d'inode disponible on va à la fin du tableau.
+  if ((i_inode = get_unused_inode()) == -1)
+    i_inode = INODE_TABLE_SIZE - 1;
+
+  // Pour avoir le premier byte libre on prend le premier byte du dernier inode + la taille du fichier.
+  r5Disk.super_block.first_free_byte = r5Disk.inodes[i_inode -1].first_byte + r5Disk.inodes[i_inode -1].size;
 }
