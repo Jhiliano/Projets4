6d992d193202c3d9e00cc14ff9d85f70101c9d19
Vibujor <35135608+Vibujor@users.noreply.github.com>
2019-04-25 21:20:32 +0200

    definitions_4
    Définitions des fonctions de la couche 4 dans le .c et dans le .h.

--- headers/Raid5/couche4.h
+++ headers/Raid5/couche4.h
@@ -16,6 +16,10 @@
 
 
 /* Fonctions */
-
+void writefile(char* nomFich, file_t fich);
+int read_file(char* nomF, file_t fich);
+int delete_file(char* nomF);
+void load_file_from_host(char* nomFich);
+void store_file_to_host(char* nomFich);
 
 #endif
--- src/Raid5/couche4.c
+++ src/Raid5/couche4.c
@@ -11,3 +11,24 @@
 
 
 /* fonctions */
+void writefile(char* nomFich, file_t fich){
+  for(int i=0; i<INODE_TABLE_SIZE; i++){
+    
+  }
+}
+
+int read_file(char* nomF, file_t fich){
+  return 1;
+}
+
+int delete_file(char* nomF){
+  return 1;
+}
+
+void load_file_from_host(char* nomFich){
+
+}
+
+void store_file_to_host(char* nomFich){
+
+}
