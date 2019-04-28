48f911bfffeaeaa02ee140d6d887ff9bc1e89f8c
Jhiliano <44864519+Jhiliano@users.noreply.github.com>
2019-04-28 20:57:35 +0200

    creation des différents objets

--- javaR5/src/javaR5/Block.java
+++ javaR5/src/javaR5/Block.java
@@ -1,5 +1,15 @@
 package javaR5;
 
 public class Block {
-
+	private char[] donnees;
+	public Block(char[] donnees, int size) {
+		this.donnees = new char[size];
+		for (int i = 0; i < size; i++) {
+			this.donnees[i] = donnees[i];
+		}
+	}
+	public char[] getDonnees() {
+		return donnees;
+	}
+	
 }
