a02f5630afde53179387ebaf513cc2214e0d5f73
Vibujor <35135608+Vibujor@users.noreply.github.com>
2019-05-04 14:01:33 +0200

    Update couche5.c

--- src/Raid5/couche5.c
+++ src/Raid5/couche5.c
@@ -11,6 +11,28 @@
 
 
 /* fonctions */
+int Decoupe(char Chaine[], char *pMots[])
+{
+  char *p;
+  int NbMots=0;
+
+  p=Chaine; /* On commence par le début */
+  /* Tant que la fin de la chaîne n'est pas atteinte et qu'on ne déborde pas */
+  while ((*p)!='\0' && NbMots<NBMOTSMAX)
+  {
+    while ((*p)==' ' && (*p)!='\0') p++; /* Recherche du début du mot */
+    if ((*p)=='\0') break; /* Fin de chaîne atteinte */
+    pMots[NbMots++]=p; /* Rangement de l'adresse du 1er caractère du mot */
+    while ((*p)!=' ' && (*p)!='\0') p++; /* Recherche de la fin du mot */
+    if ((*p)=='\0') break; /* Fin de chaîne atteinte */
+    *p='\0'; /* Marquage de la fin du mot */
+    p++; /* Passage au caractère suivant */
+  }
+  pMots[NbMots]=NULL; /* Dernière adresse */
+  return NbMots;
+}
+
+
 void interpret(int argc, char*argv[]){
   char *pMots[NBMOTSMAX+1];
 	pid_t pidF;
@@ -27,14 +49,11 @@ void interpret(int argc, char*argv[]){
 			case -1:	perror("Echec fork\n");
 						exit(1);
 			case 0:	printf("[%d] Je lance %s :\n", (int)getpid(), argv[i]);
-						Decoupe(argv[i],pMots);
-						execvp(pMots[0],pMots);
-						perror(argv[i]);
-						exit(2);
-			default:	printf("[%d] J'ai délégué %s à %d. J'attends sa fin...\n", (int)getpid(), argv[i], (int)pidF);
-						wait(NULL);
-						printf("[%d] %d terminé.\n", (int)getpid(), (int)pidF);
-						printf("[%d] J'ai fini.\n", (int)getpid());
+  						Decoupe(argv[i],pMots);
+  						execvp(pMots[0],pMots);
+  						perror(argv[i]);
+  						exit(2);
+			default: wait(NULL);
 		}
 	}
 	printf(">\n");
