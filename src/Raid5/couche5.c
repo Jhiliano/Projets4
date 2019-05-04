6d0cb9429f57ac8c04ee9f6e5758e4c264731299
Vibujor <35135608+Vibujor@users.noreply.github.com>
2019-05-04 20:00:47 +0200

    update c5.c

--- src/Raid5/couche5.c
+++ src/Raid5/couche5.c
@@ -15,26 +15,6 @@
 #define NBMOTSMAX 20
 
 /* fonctions */
-#define NBMOTSMAX 20
-
-/* fonctions */
-/// \file couche5.c
-/// \brief ficher de code couche5
-/// \author Guillaume Fourcroy
-/// \date 24 février 2019
-
-
-/* dépendances */
-
-
-#include "../../headers/Raid5/couche5.h"
-#include <sys/types.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#define NBMOTSMAX 20
-
-/* fonctions */
 int Decoupe(char Chaine[], char *pMots[])
 {
   char *p;
