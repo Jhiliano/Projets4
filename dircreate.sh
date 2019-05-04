4b3e460e370ede73f66eeb3e36e187694b23c42a
Jhiliano <ggfourcroy@gmail.com>
2019-05-04 20:45:03 +0200

    edit

--- dircreate.sh
+++ dircreate.sh
@@ -3,12 +3,15 @@ obj="obj"
 bin="bin"
 bint="tests/bin"
 
-if [ ! -n $obj ]; then
- mkdir $obj
+if [ ! -n $obj ]
+then
+  mkdir $obj
 fi
-if [ ! -n $bin ]; then
+if [ ! -n $bin ]
+then
  mkdir $bin
 fi
-if [ ! -n $bint ]; then
+if [ ! -n $bint ]
+then
  mkdir $bint
 fi
