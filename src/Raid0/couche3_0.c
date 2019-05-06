0dda1ac7aea85dd0030b3784d57d254798162013
Vibujor <35135608+Vibujor@users.noreply.github.com>
2019-05-06 12:55:20 +0200

    Update couche3_0.c

--- src/Raid0/couche3_0.c
+++ src/Raid0/couche3_0.c
@@ -11,3 +11,65 @@
 
 
 /* fonctions */
+
+void r0_init_inode(char *nomF, uint taille, uint pos) {
+  /**
+  * \brief Initialise un inode à partir de son nom, sa taille et sa position.
+  * \details Avant d'appeler init_inode il faut vérifier que nomF < FILENAME_MAX_SIZE.
+  * \param nomF Nom du fichier.
+  * \param taille Taille du fichier.
+  * \param pos  Position sur le raid.
+  */
+
+  inode_t i; // Nouveau inode.
+  int i_inode; // Position à laquelle insérer cet inode.
+  i_inode = get_unused_inode();
+  // On remplit l'inode.
+  strcpy(i.filename, nomF);
+  i.size = taille;
+  i.nblock = r0compute_nstripe(compute_nblock(taille))*4;
+  i.first_byte = pos;
+
+  // On place l'inode dans la table
+  r5Disk.inodes[i_inode] = i;
+
+  // Mise à jour le super block.
+  r5Disk.super_block.nb_blocks_used += i.nblock;
+  first_free_byte();
+  // Mise à jour du Raid
+  r5Disk.number_of_files += 1;
+}
+
+int r0_write_inodes_table(void) {
+  /**
+  * \brief Ecrit la table d'inode dans le raid.
+  * \details Sauvegarde de la table d'inodes et du super block.
+  * \return 0 si cela s'est bien passé 1 si il y a eut une erreur de lecture
+  */
+  uchar* buffer = malloc(sizeof(inode_t));
+  for (uint i = 0; i < INODE_TABLE_SIZE; i++) {// on fait l'operation suivante pour chaque inode
+    for (uint c = 0; c < FILENAME_MAX_SIZE; c++) {// on ecrit les FILENAME_MAX_SIZE premiers octets qui représente le nom
+       buffer[c] = r5Disk.inodes[i].filename[c];
+    }
+    uint_to_uchar(buffer, FILENAME_MAX_SIZE, r5Disk.inodes[i].size);// on converti et rentre les 3 uint apres FILENAME_MAX_SIZE
+    uint_to_uchar(buffer, FILENAME_MAX_SIZE + sizeof(uint), r5Disk.inodes[i].nblock);
+    uint_to_uchar(buffer, FILENAME_MAX_SIZE + sizeof(uint)*2, r5Disk.inodes[i].first_byte);
+    if(r0_write_chunk(buffer, sizeof(inode_t), INODES_START+INODE_SIZE*i*BLOCK_SIZE-1, &r5Disk)) return 1;// on ecrit dans les disques et on verifie que tout s'est bien passé
+  }
+  free(buffer);
+  return 0;
+}
+
+int r0_write_super_block(void) {
+  /**
+  * \brief Ecrit le super block au début du raid.
+  * \return 1 si il u a eut une erreur 0 sinon.
+  */
+  uchar* buffer = malloc(sizeof(super_block_t));
+  uint_to_uchar(buffer, 0, r5Disk.super_block.raid_type);// on converti et rentre les 3 uint de la structure et on les palce dans le buffer
+  uint_to_uchar(buffer, sizeof(int), r5Disk.super_block.nb_blocks_used);
+  uint_to_uchar(buffer, sizeof(int)*2, r5Disk.super_block.first_free_byte);
+  if(r0_write_chunk(buffer, SUPER_BLOCK_SIZE*BLOCK_SIZE, 0, &r5Disk)) return 1;// on ecrit le buffer dans les disques et on verifie que tout s'est bien passé
+  free(buffer);
+  return 0;
+}
